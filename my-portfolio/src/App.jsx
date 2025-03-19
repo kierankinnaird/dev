316cf124deef68f54b58582b3573a23a5753fa9c
Kieran Kinnaird <k.kinnaird@rgu.ac.uk>
2025-03-19 19:43:03 +0000

    centered text etc

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     <Router>
       <VantaBackground />
       <Navbar />
-      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen text-white">
+      <div className="fixed inset-0 flex flex-col justify-center items-center text-white text-center">
         <h1 
           className={`text-4xl font-bold transition-opacity duration-1000 ${
             showWelcome ? "opacity-100" : "opacity-0"
@@ -29,7 +29,7 @@ function App() {
           Welcome!
         </h1>
         <h2 
-          className={`text-3xl font-semibold mt-4 text-white transition-opacity duration-1000 ml-6 ${
+          className={`text-3xl font-semibold mt-4 text-white transition-opacity duration-1000 ${
             showName ? "opacity-100" : "opacity-0"
           }`}
         >
@@ -41,8 +41,6 @@ function App() {
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </div>
-      {/* Extra height to test scrolling */}
-      <div className="h-[200vh]"></div>
     </Router>
   );
 }
